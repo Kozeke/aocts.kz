655dbc9d38348493ab1a270f351c740e6fe0e765
Kozy-Korpesh Tolep <kozykorpesh.tolep@nu.edu.kz>
2020-10-20 01:58:51 +0600

    bank requisites

--- app/Http/Controllers/Api/Auth/LoginController.php
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -27,7 +27,7 @@ class LoginController extends Controller
         } catch (\Tymon\JWTAuth\Exceptions\UserNotDefinedException $e){
             return response()->json(['error'=> $e->getMessage()]);
         }
-        $me = User::where('id', auth()->user()->id)->with(['documents','agreements','bank_requisites'])->get();
+        $me = User::where('id', auth()->user()->id)->with(['documents','agreements','bank_requisites','applications'])->get();
         return response()->json(['user'=>$me]);
     }
 
