4e2c985940f9327f48341a4bce41dd830ace53d0
Kozy-Korpesh Tolep <kozykorpesh.tolep@nu.edu.kz>
2020-10-23 00:04:56 +0600

    bank req id

--- app/Agreement.php
+++ app/Agreement.php
@@ -8,6 +8,6 @@ class Agreement extends Model
 {
     protected $fillable = [
         'application_id', 'business_index', 'address','access_road','amount_of_carriage_on_loading','amount_of_carriage_on_unloading',
-        'size_of_the_simultaneous_supply_of_wagons','carriage_address','agreement_start_date','agreement_end_date','performer'
+        'size_of_the_simultaneous_supply_of_wagons','carriage_address','agreement_start_date','agreement_end_date','performer','bank_req_id'
     ];
 }
